8201209a7114797b105f360f65ab44cfa7628224
aaustinparker <austin.d.parker@vanderbilt.edu>
2025-03-22 21:16:32 +0000

    Add splash page with some filler images

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,6 +1,14 @@
+
+# housekeeping
 sudo chmod -R 777 /workspaces/website
 sudo chown -R vscode /workspaces/website
-
 sudo apt update && sudo apt upgrade
+git config --global core.autocrlf true
+
+# installation
 gem install jekyll
 bundle
+
+# aliases
+echo "alias serve='bundle exec jekyll serve'" > ~/.bash_aliases
+source ~/.bash_aliases
